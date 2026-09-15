d68ceace283d7a6b39a3fa6bfacab8c1e147ed90
aylin-021 <aylin.sh.oooo@gmail.com>
2026-09-15 14:30:20 +0330

    update logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,37 @@
+// import localFont from "next/font/local";
+// import "./globals.css";
+
+// const iranYekan = localFont({
+//   src: [
+//     {
+//       path: "./fonts/iranyekan-regular.woff2",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/iranyekan-medium.woff2",
+//       weight: "500",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/iranyekan-bold.woff2",
+//       weight: "700",
+//       style: "normal",
+//     },
+//   ],
+//   display: "swap",
+// });
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="fa" dir="rtl">
+//       <body className={`${iranYekan.className} min-h-full flex flex-col`}>
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -21,6 +55,11 @@ const iranYekan = localFont({
   ],
   display: "swap",
 });
+
+export const metadata = {
+  title: "فرادرس",
+  description: "آموزش آنلاین فرادرس",
+};
 
 export default function RootLayout({ children }) {
   return (
